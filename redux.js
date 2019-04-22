7a9d59a693f5021d299b561de57312e6e3678384
Brian Pennington <srsrec@gmail.com>
2019-04-22 13:10:37 -0500

    Completed Action and Reducer Code

--- redux.js
+++ redux.js
@@ -39,3 +39,21 @@ const claimsHistory = (listOfClaimsToUpdate = [], action) => {
     }
     return listOfClaimsToUpdate;
 };
+
+const account = (totalAmount = 100, action) => {
+    if (action.type === 'CREATE_CLAIM') {
+        return totalAmount - action.payload.amountToCollect;
+    } else if (action.type === 'CREATE_POLICY') {
+        return totalAmount + action.payload.amount;
+    }
+    return totalAmount;
+};
+
+const policies = (listOfPolicies = [], action) => {
+    if (action.type === 'CREATE_POLICY') {
+        return [...listOfPolicies, action.payload.name];
+    } else if (action.type === 'DELETE_POLICY') {
+        return listOfPolicies.filter(name => name !== action.payload.name);
+    }
+    return listOfPolicies;
+};
